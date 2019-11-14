b8ccc4216221e2dcfc5737b4fab6f1fb3de3dbab
Jordan <jordan@burdett.us>
2019-11-14 15:07:34 -0700

    test

--- index.js
+++ index.js
@@ -13,17 +13,132 @@ express()
   .post('/mailCost', calcMailCost)
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+  function calcStamp(weight) {
+    let answer = 0;
+    if (weight <= 1) {
+      answer = 0.55;
+    }
+    else if(weight <= 2) {
+      answer = 0.70;
+    }
+    else if(weight <= 3) {
+      answer = 0.85;
+    }
+    else if (weight <= 3.5) {
+      answer = 1.00;
+    }
+    else {
+      console.log("ERROR invalid weight " + weight);
+      answer = 100000000;
+    }
+
+    return answer;
+  }
+
+  function calcMetered(weight) {
+    let answer = 0;
+    if (weight <= 1) {
+      answer = 0.50;
+    }
+    else if(weight <= 2) {
+      answer = 0.65;
+    }
+    else if(weight <= 3) {
+      answer = 0.80;
+    }
+    else if (weight <= 3.5) {
+      answer = 0.95;
+    }
+    else {
+      console.log("ERROR");
+      answer = 100000000;
+    }
+
+    return answer;
+  }
+
+  function calcFlat(weight) {
+    let answer = 0;
+    if (weight <= 1) {
+      answer = 1.00;
+    } else if (weight <= 2) {
+      answer = 1.15;
+    } else if (weight <= 3) {
+      answer = 1.30;
+    } else if (weight <= 4) {
+      answer = 1.45;
+    } else if (weight <= 5) {
+      answer = 1.60;
+    } else if (weight <= 6) {
+      answer = 1.75;
+    } else if (weight <= 7) {
+      answer = 1.90;
+    } else if (weight <= 8) {
+      answer = 2.05;
+    } else if (weight <= 9) {
+      answer = 2.20;
+    } else if (weight <= 10) {
+      answer = 2.35;
+    } else if (weight <= 11) {
+      answer = 2.50;
+    } else if (weight <= 12) {
+      answer = 2.65;
+    } else if (weight <= 13) {
+      answer = 2.80;
+    } else {
+      console.log("ERROR invalid weight");
+      answer = 1000000;
+    }
+
+    return answer;
+  }
+
+  function calcFirstClass(weight) {
+    let answer = 0;
+    if (weight <= 4) {
+      answer = 3.66;
+    } else if (weight <= 8) {
+      answer = 4.39;
+    } else if (weight <= 12) {
+      answer = 5.19;
+    } else if (weight <= 13) {
+      answer = 5.71;
+    } else {
+      console.log("ERROR invalid weight");
+      answer = 1000000;
+    }
+
+    return answer;
+  }
+
 
 
   function calcMailCost(requests, response) {
     console.log(requests.body);
     let weight = requests.body.weight;
     let type   = requests.body.type;
+    let answer;
 
+    switch(type) {
+      case "Letters (Stamped)":
+        answer = calcStamp(weight);
+      break;
+      case "Letters (Metered)":
+        answer = calcMetered(weight);
+      break;
+      case "Large Envelopes (Flats)":
+        answer = calcFlat(weight);
+      break;
+      case "First-Class Package Service—Retail":
+        answer = calcFirstClass(weight);
+      break;
+      default:
+        console.log("ERROR invalid type");
+      break;
 
-    var answer = weight;
+    }
 
     response.writeHead(200, {'Content-Type' : 'text/html'});
-    response.write('<h1>' + answer + '</h1>');
+    response.write('<a href="/calculate"><h1><i class="fas fa-envelope fa-md"></i> The total cost will be $' + answer + '</h1></a>');
     response.end();
   }
